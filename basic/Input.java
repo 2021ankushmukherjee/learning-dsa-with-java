9706de5ccfdf993bedfa01bc8113250d75e7f287
2021ankushmukherjee <ankush.mukherjee24@gmail.com>
2025-04-28 14:01:40 +0530

    test

---
+++ basic/Input.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Input {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number");
+        int a = sc.nextInt();
+
+        System.out.println("Enter a number");
+        int b = sc.nextInt();
+
+        System.out.println("Total number is");
+        System.out.println(a + b);
+    }
+}
