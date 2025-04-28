111c32c710574bd30e9dc7c15feece7322c9b01c
2021ankushmukherjee <ankush.mukherjee24@gmail.com>
2025-04-28 14:02:04 +0530

    test

---
+++ basic/Loop.java
@@ -0,0 +1,38 @@
+public class Loop {
+    public static void main(String[] args) {
+        // for (int i = 0; i < 10; i++) {
+        // System.out.println("Hello World");
+        // }
+
+        // int i = 0;
+        // while (i < 10) {
+        // System.out.println("Hello World");
+        // i++;
+        // }
+
+        // int j = 0;
+        // do {
+        // System.out.println("Hello World");
+        // j++;
+        // } while (j < 10);
+
+        // int sum = 0;
+        // for (int i = 1; i <= n; i++) {
+        // sum += i;
+        // }
+        // System.out.println(sum);
+
+        // int n = 12;
+        // for (int i = 1; i <= n; i++) {
+        //     System.out.println(n + " * " + i + " = " + n * i);
+        // }
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
