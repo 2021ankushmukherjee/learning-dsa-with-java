600df7238b7a8ac2ac64a6ca7427146a0439b2f7
2021ankushmukherjee <ankush.mukherjee24@gmail.com>
2025-04-28 13:16:08 +0530

    test

---
+++ basic/FirstClass.java
@@ -0,0 +1,24 @@
+class FirstClass {
+
+    public static void main(String[] args) {
+        // System.out.println("Hello World");
+        System.out.println("*");
+        // primitive data types
+
+        byte a = 10;
+        short b = 10;
+        int c = 10;
+        long d = 10;
+        float e = 10;
+        double f = 10;
+        char g = 'a';
+        boolean h = true;
+
+        // non primitive
+
+        String s = "hello";
+        int[] arr = { 1, 2, 3, 4, 5 };
+        int[][] arr2 = { { 1, 2, 3 }, { 4, 5, 6 } };
+        // class object interface
+    }
+}
