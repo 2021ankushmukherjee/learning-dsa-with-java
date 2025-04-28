34b995e8dade428ef1674efeb751821ddcb16151
2021ankushmukherjee <ankush.mukherjee24@gmail.com>
2025-04-28 13:51:50 +0530

    test

---
+++ basic/Functions.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Functions {
+
+    private static int calculateProduct(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+     System.out.println("Result is " + calculateProduct(a, b));
+
+    }
+
+}
