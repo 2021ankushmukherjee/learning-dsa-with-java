290e8735e29b2878592045a84cee2dcb3105ea8d
2021ankushmukherjee <ankush.mukherjee24@gmail.com>
2025-04-28 14:35:41 +0530

    test

--- basic/Input.java
+++ basic/Input.java
@@ -8,7 +8,6 @@ public class Input {
 
         System.out.println("Enter a number");
         int a = sc.nextInt();
-
         
         System.out.println("Enter a number");
         int b = sc.nextInt();
